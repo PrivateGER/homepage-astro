7398752ac6a9c28bd1b0bbd4560764954092827d
PrivateGER <privateger@privateger.me>
2024-02-09 10:05:20 +0100

    optimize images + vercel SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    imagesConfig: {
+      sizes: [320, 640, 1280, 1920]
+    }
+  })
+});
