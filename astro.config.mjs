b79b901a5ad34c002f28445bf04f2f06709e1904
PrivateGER <privateger@privateger.me>
2024-02-10 22:18:56 +0000

    Add fediring and modify gallery page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
-
 import icon from "astro-icon";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel({
-    imageService: true,
+    imageService: true
   }),
-  integrations: [icon()]
+  integrations: [icon(), mdx()]
 });
